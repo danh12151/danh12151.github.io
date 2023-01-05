6f01dab3b2ff1b6b5f00ce5782ae3abbfe6dbb43
Le Ngoc Danh <lengocdanh123456@gmail.com>
2023-01-05 18:52:43 +0700

    update first page

--- app.js
+++ app.js
@@ -10,4 +10,10 @@ const observer = new IntersectionObserver( (entries) => {
 });
 
 const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenLeftElements = document.querySelectorAll(".hidden-left");
+const hiddenRightElements = document.querySelectorAll(".hidden-right");
+const logoElements = document.querySelectorAll(".logo");
 hiddenElements.forEach( (el) => observer.observe(el));
+hiddenLeftElements.forEach( (el) => observer.observe(el));
+hiddenRightElements.forEach( (el) => observer.observe(el));
+logoElements.forEach( (el) => observer.observe(el));
